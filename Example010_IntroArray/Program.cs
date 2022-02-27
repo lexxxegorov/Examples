72b51a9fd09961bd660e466ffaa1feacae60c1ed
Aleksey <egorov.axl@gmail.com>
2022-02-27 11:45:05 +0300

    Example 010 IntroArray

---
+++ Example010_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 13, 47, 55, 65, 71, 18};
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
